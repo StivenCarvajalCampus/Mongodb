55025efba023724b9228b5367cf55e18f0b3c610
Stiven Carvajal <StivenCarvajal3211@gmail.com>
2023-08-09 16:56:38 -0500

    feat(MongoDb): :sparkles: estructura

---
+++ db/atlas.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+dotenv.config("../");
+
+export async function conex(){
+    try {
+        const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.nfquokb.mongodb.net/${process.env.ATLAS_DB}`
+        const options={
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+        };
+        const client =await MongoClient.connect(uri, options);
+        return client.db();
+
+
+    } catch (error) {
+        return {status: 500, message:error.message}
+    }
+}
